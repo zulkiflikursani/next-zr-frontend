b3c966643748ef0c19148884f2860caf6893453a
zulkiflikursani <69183966+zulkiflikursani@users.noreply.github.com>
2024-04-09 02:11:48 +0800

    date makassar

--- src/app/dashboard/(overview)/penjualan/page.tsx
+++ src/app/dashboard/(overview)/penjualan/page.tsx
@@ -4,11 +4,23 @@ import React from "react";
 
 async function penjualan() {
   const currentDate = new Date();
-  const year = currentDate.getFullYear();
-  const month = currentDate.getMonth() + 1; // Months are zero-based, so we add 1
-  const day = currentDate.getDate();
+  // Set the timezone to Makassar, Indonesia (WITA)
+  const options: Intl.DateTimeFormatOptions = {
+    timeZone: "Asia/Makassar",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    // hour: "2-digit",
+    // minute: "2-digit",
+    // second: "2-digit",
+    // hour12: false,
+  };
+  const formattedDate = currentDate.toLocaleDateString("en-US", options);
+  // const year = currentDate.getFullYear();
+  // const month = currentDate.getMonth() + 1; // Months are zero-based, so we add 1
+  // const day = currentDate.getDate();
 
-  const formattedDate = `${year}-${month}-${day}`;
+  // const formattedDate = `${year}-${month}-${day}`;
   const dataPenjualan = await getPenjualanByDate(formattedDate);
   return (
     <div className="h-[80vh]">
